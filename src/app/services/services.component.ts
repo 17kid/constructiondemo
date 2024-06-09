e9428905cbb57045fcf24459689a8829eb4386fe
Jared Johnson <jaredjohnson@Jareds-MacBook-Pro.local>
2024-06-08 23:11:39 -0400

    adding routing, editing contact page

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html'
 })
 export class ServicesComponent {
+
+  @Output() contactClick = new EventEmitter();
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(x => {
+      if (x instanceof NavigationEnd)
+      {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+  contactNavigation() {
+    this.contactClick.emit('button was clicked to navigate to home');
+    this.router.navigate(['/contact']);
+  }
 
 }
